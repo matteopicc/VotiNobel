05bd2ffac842ade4b061c6425bf6bf0868d55dce
Matteo <Matteo@ASUS-7812A.lan>
2022-05-30 16:25:32 +0200

    aggiornamento

--- src/main/java/it/polito/tdp/nobel/model/Model.java
+++ src/main/java/it/polito/tdp/nobel/model/Model.java
@@ -8,115 +8,116 @@ import it.polito.tdp.nobel.db.EsameDAO;
 
 public class Model {
 	
-	private List<Esame>esami;
-	private Set<Esame>migliore;
+	private List<Esame> esami;
+	private Set<Esame> migliore;
 	private double mediaMigliore;
 	
 	public Model() {
 		EsameDAO dao = new EsameDAO();
 		this.esami = dao.getTuttiEsami();
+		
 	}
 	
-	public Set<Esame> calcolaSottoinsiemeEsami(int m) {
-		//ripristino soluzione migliore ad ogni richiesta 
+	public Set<Esame>calcolaSottoinsiemeEsami(int m){
+		// ripristino soluzione migliore
 		migliore = new HashSet<Esame>();
 		mediaMigliore = 0.0;
 		
 		Set<Esame>parziale = new HashSet<Esame>();
 		//cerca1(parziale,0,m);
 		cerca2(parziale,0,m);
-		
-		
-		
-		return migliore;	
+		return migliore;
 	}
+	
 	private void cerca2(Set<Esame> parziale, int L, int m) {
-		//Controllare i casi terminali, altrimenti loop infinito
-		
+		//Controllare i casi terminali
 				int sommaCrediti = sommaCrediti(parziale);
-				if(sommaCrediti>m) {
-					return;  // soluzione non valida
+				
+				if(sommaCrediti > m) {// soluzione non valida
+					return;
 				}
+				
 				else if(sommaCrediti == m) {
-					// soluzione valida, controlliamo se è la migliore finora
+					// soluzione valida! controlliamo se è la migliore(fino a qui)
 					double mediaVoti = calcolaMedia(parziale);
+					
 					if(mediaVoti > mediaMigliore) {
-						migliore = new HashSet<Esame>(parziale); //voglio una copia, non un riferimento
-						mediaMigliore = mediaVoti;
+						migliore = new HashSet(parziale) ;
+						mediaMigliore = mediaVoti;	
 					}
 					return;
 				}
-				// sicuremente i crediti qui sono < di m
-				if(L == esami.size()) {
+				// sicuramente qui i crediti sono minori di m
+				if (L == esami.size()) {
 					return;
 				}
+				//provo ad aggiungere esami[L]
+				parziale.add(esami.get(L));
+				cerca2(parziale,L+1,m);
 				
-	// provo ad aggiungere esami[L]
-	parziale.add(esami.get(L));
-	cerca2(parziale,L+1,m);
 				
-	
-    // provo a non aggiungere esami[l]
-	parziale.remove(esami.get(L));
-	cerca2(parziale,L+1,m);
+				//provo a NON aggiungere esami[L]
+				parziale.remove(esami.get(L));
+				cerca2(parziale,L+1,m);
 		
 	}
 
-	/*
-	 * COMPLESSITA N!
-	 */
-
-	
-	private void cerca1(Set<Esame> parziale, int L, int m) {
-		//Controllare i casi terminali, altrimenti loop infinito
-		
+	private void cerca1(Set<Esame> parziale, int L, int m) {//COMPLESSITA' N!(MOOOOLTO ELEVATA)
+		//Controllare i casi terminali
 		int sommaCrediti = sommaCrediti(parziale);
-		if(sommaCrediti>m) {
-			return;  // soluzione non valida
-		}
-		else if(sommaCrediti == m) {
-			// soluzione valida, controlliamo se è la migliore finora
-			double mediaVoti = calcolaMedia(parziale);
-			if(mediaVoti > mediaMigliore) {
-				migliore = new HashSet<Esame>(parziale); //voglio una copia, non un riferimento
-				mediaMigliore = mediaVoti;
-			}
+		
+		if(sommaCrediti > m) {// soluzione non valida
 			return;
 		}
-		// sicuremente i crediti qui sono < di m
-		if(L == esami.size()) {
+		
+		else if(sommaCrediti == m) {
+			// soluzione valida! controlliamo se è la migliore(fino a qui)
+			double mediaVoti = calcolaMedia(parziale);
+			
+			if(mediaVoti > mediaMigliore) {
+				migliore = new HashSet(parziale) ;
+				mediaMigliore = mediaVoti;
+				
+			}
+			
+			return;
+		}
+		
+		// sicuramente qui i crediti sono minori di m
+		if (L == esami.size()) {
 			return;
 		}
 		//ora generiamo i sotto-problemi
+		
 		for(Esame e : esami) {
-			if(! parziale.contains(e)) {
+			if(!parziale.contains(e)) {
 				parziale.add(e);
 				cerca1(parziale,L+1,m);
 				parziale.remove(e);
 			}
 		}
+		
 	}
 
 	public double calcolaMedia(Set<Esame> esami) {
-		
 		int crediti = 0;
 		int somma = 0;
 		
-		for(Esame e : esami){
+		for(Esame e : esami) {
 			crediti += e.getCrediti();
-			somma += (e.getVoto() * e.getCrediti());
+			somma += (e.getVoto()*e.getCrediti());
 		}
-		
 		return somma/crediti;
 	}
 	
 	public int sommaCrediti(Set<Esame> esami) {
 		int somma = 0;
 		
-		for(Esame e : esami)
-			somma += e.getCrediti();
-		
-		return somma;
+		for(Esame e : esami) {
+			somma +=e.getCrediti();
+		}
+		return somma;	
 	}
-
+	
+	
 }
